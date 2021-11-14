597c006af5195a6f587dec8b37843aa8d78b4965
capn-21 <chalkeom99@gmail.com>
2021-11-14 21:31:57 +0530

    2nd

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 console.log("inside index .js")
 
-function book()
+/constructor
+function Book(name,author,genre){
+this.name=name;
+this.author=author;
+this.genre=genre;
 
+}
+
+//output const
+
+function display(){
+
+}
+
+
+
+
+//submit onclick listner
+let libraryform =document.getElementById('libraryform');
+libraryform.addEventListener('submit',libraryformsubmit);
+
+
+function libraryformsubmit(e){
+    e.preventDefault();
+    console.log(' sumitbutton pressed');
+}
 
 
