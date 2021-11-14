5fad2f9118a81a575c6167d8e7ad08f785afaef5
capn-21 <chalkeom99@gmail.com>
2021-11-14 22:32:49 +0530

    3

--- index.js
+++ index.js
@@ -10,12 +10,20 @@ this.genre=genre;
 
 //output const
 
-function display(){
+function Display(){
 
 }
 
+Display.prototype.add=function(){
+    console.log("adding")
+}
 
+Display.prototype.clear=function(){
+    let libraryform =document.getElementById('libraryform');
 
+    libraryform.reset();
+
+}
 
 //submit onclick listner
 let libraryform =document.getElementById('libraryform');
@@ -25,6 +33,35 @@ libraryform.addEventListener('submit',libraryformsubmit);
 function libraryformsubmit(e){
     e.preventDefault();
     console.log(' sumitbutton pressed');
+    let name = document.getElementById('bookname').value;
+    let author=document.getElementById('Author').value;
+
+    let fiction=document.getElementById('fiction');
+    let education=document.getElementById('Education');
+
+    let Poetry=document.getElementById('Poetry');
+let genre;
+
+    if (fiction.checked){
+        genre=fiction.value;
+    }
+    else if(education.checked){
+        genre=education.value;
+    }
+    else if(Poetry,checked){
+        genre=Poetry.value;
+    }
+
+
+    var book=new Book(name,author,genre);
+    console.log(book);
+   
+
+    let display =new Display();
+    display.add(book);
+    display.clear();
+   
+  
 }
 
 
